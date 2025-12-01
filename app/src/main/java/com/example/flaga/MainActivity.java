e3673eecf1961f472f4085029f4347c5472b7df2
bvrt3kk <danusek12345@gmail.com>
2025-12-01 15:20:36 +0100

    gotowa aplikacja

--- app/src/main/java/com/example/flaga/MainActivity.java
+++ app/src/main/java/com/example/flaga/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.flaga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int licznik = 0;
+    TextView textViewPolecenie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPolecenie = findViewById(R.id.textViewPolecenie);
+    }
+
+    public void dobry(View view) {
+        Toast.makeText(this, R.string.ostrzezenie, Toast.LENGTH_SHORT).show();
+    }
+
+    public void zly(View view) {
+        view.setVisibility(View.INVISIBLE);
+        licznik++;
+        if(licznik == 4){
+            textViewPolecenie.setText(R.string.podsumowanie);
+        }
     }
 }
